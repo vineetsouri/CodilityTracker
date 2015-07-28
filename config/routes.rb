e2152da128f96ab5a7a094c4c794bc24e25ebf09
vineetsouri <vineetsouri72@gmail.com>
2015-07-28 12:05:53 +0530

    added scope and changes into call_back controller files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   authenticated :user do
     root :to => 'home#dashboard', :as => :authenticated_root
   end
   root :to => redirect('/land')
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
